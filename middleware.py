ea5d545f5771e97b471b66d1bebad320bf3a4224
Muhammad Hilman <hlmn.hg@gmail.com>
2017-05-21 11:50:52 +0700

    coba

--- middleware.py
+++ middleware.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import operator
+
 try:
     import queue
 except ImportError:
@@ -44,28 +44,52 @@ class DispatcherQueue(object):
                     print(i)
         return a
 
-    def makefile(self, currdir):
-        serversize = {}
+    def changedirectory(self, currdir, server = None):
+        a = False
         for server in self.serverlist:
             with Pyro4.Proxy(server) as storage:
-                serversize[server] = storage.size()
-        sorted_x = sorted(serversize.items(), key=operator.itemgetter(1))
-        # print (sorted_x[0][0])
-        with Pyro4.Proxy(sorted_x[0][0]) as storage:
-            storage.makefile(currdir)
+                if storage.checkdir(currdir):
+                    print("kontol")
+                    a = True
 
         return a
 
-        # print (serversize)
-    # def changedirectory(self, currdir, server = None):
-    #     for server in self.serverlist:
-    #         with Pyro4.Proxy(server) as storage:
-    #             for i in storage.listdir(currdir):
-    #                 if i == "server.py":
-    #                     continue
-    #                 a.append(i)
-    #     return 
+    def check(self, currdir, storage = None):
+        return storage.check(currdir)
 
+
+    def removefile(self, currdir):
+        a = False
+        for server in self.serverlist:
+            with Pyro4.Proxy(server) as storage:
+                if storage.checkfile(currdir):
+                    print("kontol")
+                    storage.removefile(currdir)
+                    a = True
+
+        return a
+
+    def removedir(self, currdir):
+        a = False
+        for server in self.serverlist:
+            with Pyro4.Proxy(server) as storage:
+                if storage.checkdir(currdir):
+                    print("kontol")
+                    storage.removedir(currdir)
+                    a = True
+
+        return a
+
+
+
+
+
+
+                
+
+        
+
+    
 
 # main program
 
